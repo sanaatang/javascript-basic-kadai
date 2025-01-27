ab52e64cd83999edcfd7c85c178039aea6298ed2
sanaatang <sanaatang@gmail.com>
2025-01-28 01:27:37 +0900

    Delete event.js

--- event.js
+++
@@ -1,6 +0,0 @@
-const textElement = document.getElementById('text');
-const btnElement = document.getElementById('btn');
-
-btnElement.addEventListener('click', () => {
-  textElement.textContent = 'ボタンをクリックしました';
-});
